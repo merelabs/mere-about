e97af11be430701e8d57b0253289c8e45e6adc20
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2019-11-11 18:44:25 +0600

    Use mere-utils for i18n support

--- src/mereaboutapp.cpp
+++ src/mereaboutapp.cpp
@@ -1,6 +1,9 @@
 #include "mereaboutapp.h"
 #include "mereaboutwin.h"
 
+#include "mere/utils/mereapputils.h"
+#include "mere/utils/merei18nutils.h"
+
 #include <QFileInfo>
 #include <QLocale>
 
@@ -18,57 +21,17 @@ MereAboutApp::MereAboutApp(int &argc, char **argv)
 {
     setObjectName("MereAboutApp");
 
+    MereAppUtils::setAppCode(Mere::About::AppCode);
+    setApplicationName(Mere::About::AppName);
+    setApplicationVersion(Mere::About::AppVersion);
+
     // Apply Styles
     QFile File(":/about/about.qss");
     File.open(QFile::ReadOnly);
     QString StyleSheet = QLatin1String(File.readAll());
     setStyleSheet(StyleSheet);
 
-    // Apply I18n
-    // moved the functionality to I18nUtils
-    // I18nUtils::apply();
-    // I18nUtils::apply(locale, default);
-    {
-        QString pattern = "/usr/local/share/mere/about/i18n/mere-about_%1.qm";
-
-        QLocale locale = QLocale::system();
-        QString path = pattern.arg(locale.name());
-
-        QFileInfo file(path);
-        if (!file.exists())
-        {
-            qDebug() << QString("WARN: Failed to load i18n resource file for locale - %1").arg(path);
-
-            // FALLBACK!
-            {
-                QStringList paths;
-                paths << pattern.arg(locale.languageToString(locale.language()))
-                      << pattern.arg("en_US")
-                      << pattern.arg("en");
-
-                QStringListIterator it(paths);
-                while (it.hasNext())
-                {
-                    path = it.next();
-                    file.setFile(path);
-                    if (file.exists()) break;
-                    qDebug() << QString("WARN: Failed to load i18n resource file for fallback - %1").arg(file.absoluteFilePath()) << file.exists();
-                }
-            }
-        }
-
-        if (file.exists())
-        {
-            if(m_translator.load(path))
-            {
-                QApplication::installTranslator(&m_translator);
-            }
-            else
-            {
-                qDebug() << "FATAL: Failed to load i18n resource file.";
-            }
-        }
-    }
+    MereI18nUtils::apply();
 
     m_win = new MereAboutWin();
 }
--- src/mereaboutapp.h
+++ src/mereaboutapp.h
@@ -5,6 +5,31 @@
 #include <QTranslator>
 #include <QApplication>
 
+namespace Mere
+{
+    namespace About
+    {
+        #ifdef APP_CODE
+        const QString AppCode   = APP_CODE;
+        #else
+        const QString AppCode   = "about";
+        #endif
+
+        #ifdef APP_NAME
+        const QString AppName   = APP_NAME;
+        #else
+        const QString AppName   = "mere-about";
+        #endif
+
+        #ifdef APP_VERSION
+        const QString AppVersion= APP_VERSION;
+        #else
+        const QString AppVersion= "0.0.0b";
+        #endif
+    }
+}
+
+
 class MereAboutWin;
 class MereAboutApp : public QApplication
 {
