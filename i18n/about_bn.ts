fe1567c28e5539b9f90589d322f8bbdc3b791324
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2021-03-05 12:06:18 +0600

    ..

---
+++ i18n/about_bn.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bn_BD">
+<context>
+    <name>AboutWin</name>
+    <message>
+        <source>MereAboutVersion</source>
+        <translation type="unfinished">0.0.1ব</translation>
+    </message>
+    <message>
+        <source>MereAboutSummary</source>
+        <translation type="unfinished">স্ক্র্যাচ থেকে অপারেটিং সিস্টেমের মতো ইউনিক্সের জন্য সাধারণ সিস্টেম ইন্টারফেস।
+
+বিএসডি 2-ধারা লাইসেন্সের অধীনে আমার লাইসেন্স দেওয়া হয়েছে।</translation>
+    </message>
+    <message>
+        <source>MereContactUs</source>
+        <translation type="unfinished">যোগাযোগ করুন</translation>
+    </message>
+</context>
+<context>
+    <name>MereAboutWin</name>
+    <message>
+        <source>MereAboutVersion</source>
+        <translation type="vanished">0.0.1ব</translation>
+    </message>
+    <message>
+        <source>MereAboutSummary</source>
+        <translation type="vanished">স্ক্র্যাচ থেকে অপারেটিং সিস্টেমের মতো ইউনিক্সের জন্য সাধারণ সিস্টেম ইন্টারফেস।
+
+বিএসডি 2-ধারা লাইসেন্সের অধীনে আমার লাইসেন্স দেওয়া হয়েছে।</translation>
+    </message>
+    <message>
+        <source>MereContactUs</source>
+        <translation type="vanished">যোগাযোগ করুন</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/about_en.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AboutWin</name>
+    <message>
+        <source>MereAboutVersion</source>
+        <translation type="unfinished">0.0.1b</translation>
+    </message>
+    <message>
+        <source>MereAboutSummary</source>
+        <translation type="unfinished">Simple system interface for UNIX like operating system developed from scratch.
+
+MERE is licenced under the BSD 2-clause licence.</translation>
+    </message>
+    <message>
+        <source>MereContactUs</source>
+        <translation type="unfinished">Contact Us</translation>
+    </message>
+</context>
+<context>
+    <name>MereAboutWin</name>
+    <message>
+        <source>MereAboutVersion</source>
+        <translation type="vanished">0.0.1b</translation>
+    </message>
+    <message>
+        <source>MereAboutSummary</source>
+        <translation type="vanished">Simple system interface for UNIX like operating system developed from scratch.
+
+MERE is licenced under the BSD 2-clause licence.</translation>
+    </message>
+    <message>
+        <source>MereContactUs</source>
+        <translation type="vanished">Contact Us</translation>
+    </message>
+</context>
+</TS>
